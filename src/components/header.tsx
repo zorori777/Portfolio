28e57e488d584044426151c1f25387fe6f193355
Watanabe Takumi <e2fde7ded7anxbne05se@yahoo.co.jp>
2021-02-21 23:32:24 +0900

    Fix design

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -5,10 +5,10 @@ import { useRouter } from 'next/router'
 import styles from '../styles/header.module.css'
 
 const navItems: { label: string; page?: string; link?: string }[] = [
-  { label: 'Home', page: '/' },
+  // { label: 'Home', page: '/' },
   { label: 'Blog', page: '/blog' },
-  { label: 'Contact', page: '/contact' },
-  { label: 'Source Code', link: 'https://github.com/ijjk/notion-blog' },
+  // { label: 'Contact', page: '/contact' },
+  // { label: 'Source Code', link: 'https://github.com/ijjk/notion-blog' },
 ]
 
 const ogImageUrl = 'https://notion-blog.now.sh/og-image.png'
@@ -19,7 +19,7 @@ export default ({ titlePre = '' }) => {
   return (
     <header className={styles.header}>
       <Head>
-        <title>{titlePre ? `${titlePre} |` : ''} My Notion Blog</title>
+        <title>{titlePre ? `${titlePre} |` : ''} Portfolio</title>
         <meta
           name="description"
           content="An example Next.js site using Notion for the blog"
